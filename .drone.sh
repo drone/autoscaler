52bb4add8d3d6c296428c4d280215cdbf52a8dc2
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-03-08 09:22:32 -0800

    update Dockerfile

--- .drone.sh
+++ .drone.sh
@@ -7,5 +7,5 @@ go build \
     -ldflags '-extldflags "-static"' \
 	-ldflags "-X main.version=${DRONE_TAG=latest}" \
 	-ldflags "-X main.commit=${DRONE_COMMIT_SHA}" \
-	-o release/linux/arm64/drone-autoscaler \
+	-o release/linux/amd64/drone-autoscaler \
 	github.com/drone/autoscaler/cmd/drone-autoscaler
--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ ENV XDG_CACHE_HOME /data
 ENV DATABASE_DRIVER sqlite3
 ENV DATABASE_DATASOURCE /data/database.sqlite?cache=shared&mode=rwc&_busy_timeout=9999999
 
-ADD release/linux/arm64/drone-autoscaler /bin/
+ADD release/linux/amd64/drone-autoscaler /bin/
 ENTRYPOINT ["/bin/drone-autoscaler"]

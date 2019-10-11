76ec83a71e4fa3e7c1b34638bd6d258b20863038
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-10-10 20:36:34 -0700

    add feature flag for alternate aws terminate

--- drivers/amazon/destroy.go
+++ drivers/amazon/destroy.go
@@ -6,6 +6,7 @@ package amazon
 
 import (
 	"context"
+	"os"
 
 	"github.com/drone/autoscaler"
 
@@ -16,6 +17,10 @@ import (
 )
 
 func (p *provider) Destroy(ctx context.Context, instance *autoscaler.Instance) error {
+	if os.Getenv("DRONE_FLAG_ALTERNATE_DESTROY") == "true" {
+		return p.destroy2(ctx, instance)
+	}
+
 	logger := log.Ctx(ctx).With().
 		Str("id", instance.ID).
 		Str("ip", instance.Address).
@@ -52,4 +57,71 @@ func (p *provider) Destroy(ctx context.Context, instance *autoscaler.Instance) e
 		Msg("terminated")
 
 	return nil
+}
+
+func (p *provider) destroy2(ctx context.Context, instance *autoscaler.Instance) error {
+	logger := log.Ctx(ctx).With().
+		Str("id", instance.ID).
+		Str("ip", instance.Address).
+		Str("name", instance.Name).
+		Str("zone", instance.Region).
+		Logger()
+
+	logger.Debug().
+		Msg("terminate instance")
+
+	input := &ec2.TerminateInstancesInput{
+		InstanceIds: []*string{
+			aws.String(instance.ID),
+		},
+	}
+	_, err := p.getClient().TerminateInstances(input)
+	if err == nil {
+		logger.Debug().Msg("terminated")
+		return nil
+	}
+
+	// if terminate instance returns an error indicating
+	// the instance no longer exists, return a not found
+	// error.
+	if awsErr, ok := err.(awserr.Error); ok {
+		switch awsErr.Code() {
+		case ec2.UnsuccessfulInstanceCreditSpecificationErrorCodeInvalidInstanceIdMalformed:
+			logger.Debug().Msg("instance does not exist")
+			return autoscaler.ErrInstanceNotFound
+		case ec2.UnsuccessfulInstanceCreditSpecificationErrorCodeInvalidInstanceIdNotFound:
+			logger.Debug().Msg("instance does not exist")
+			return autoscaler.ErrInstanceNotFound
+		}
+	}
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("cannot terminate instance")
+	}
+
+	logger.Debug().
+		Msg("describe instance")
+
+	describe := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{
+			aws.String(instance.ID),
+		},
+	}
+	_, err = p.getClient().DescribeInstances(describe)
+	if awsErr, ok := err.(awserr.Error); ok {
+		switch awsErr.Code() {
+		case ec2.UnsuccessfulInstanceCreditSpecificationErrorCodeInvalidInstanceIdMalformed:
+			logger.Debug().Msg("instance does not exist")
+			return autoscaler.ErrInstanceNotFound
+		case ec2.UnsuccessfulInstanceCreditSpecificationErrorCodeInvalidInstanceIdNotFound:
+			logger.Debug().Msg("instance does not exist")
+			return autoscaler.ErrInstanceNotFound
+		}
+	}
+
+	logger.Error().
+		Err(err).
+		Msg("cannot describe instance")
+	return err
 }

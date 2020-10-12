a9d092958cd2d6a3de88e3919fd9414029b1902b
Matti R <matti@mdranta.net>
2020-10-12 03:45:28 -0400

    config env var

--- engine/planner.go
+++ engine/planner.go
@@ -153,7 +153,7 @@ func (p *planner) mark(ctx context.Context, n int) error {
 		logger.WithField("servers-to-terminate", n).
 			WithField("servers-running", len(servers)).
 			WithField("min-pool", p.min).
-			Debugf("abort terminating %d instances to ensure minimum capacity met")
+			Debugf("abort terminating %d instances to ensure minimum capacity met", n)
 		return nil
 	}
 

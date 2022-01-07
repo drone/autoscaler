58887519084dd3e31a3abef8547aec9cadc55301
Evan Smith <esmith@squarespace.com>
2022-01-07 16:11:11 +0000

    Remove google zone env config

--- cmd/drone-autoscaler/main.go
+++ cmd/drone-autoscaler/main.go
@@ -244,7 +244,6 @@ func setupProvider(c config.Config) (autoscaler.Provider, error) {
 			google.WithUserData(c.Google.UserData),
 			google.WithUserDataFile(c.Google.UserDataFile),
 			google.WithZones(c.Google.Zone),
-			google.WithRegion(c.Google.Region),
 		)
 	case c.DigitalOcean.Token != "":
 		return digitalocean.New(

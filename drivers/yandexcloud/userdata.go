03ce0a6ca1cc6523ad39dfe9173622cb95e54ec5
Александр Наумочкин <66272021+SobakaBaka@users.noreply.github.com>
2024-05-31 15:08:35 +0300

    feat(proxy): add proxy to deamon.json

--- drivers/yandexcloud/userdata.go
+++ drivers/yandexcloud/userdata.go
@@ -22,6 +22,7 @@ write_files:
     encoding: b64
     content: {{ .TLSKey | base64 }}
 
+
 {{ if .SSHUser | eq "" | not }}
 users:
   - name: {{ .SSHUser }}
@@ -37,6 +38,7 @@ users:
 runcmd:
   - sudo sed -i "s/ExecStart=\\/usr\\/bin\\/dockerd/ExecStart=\\/usr\\/bin\\/dockerd -H tcp:\\/\\/0.0.0.0:2376 --tlsverify --tlscacert=\\/etc\\/docker\\/ca.pem --tlskey=\\/etc\\/docker\\/key.pem --tlscert=\\/etc\\/docker\\/cert.pem/g" /lib/systemd/system/docker.service
   - sudo sed -i "s/DOCKER_OPTS=.*/DOCKER_OPTS=\"-H tcp:\\/\\/0.0.0.0:2376\"/g" /etc/init.d/docker
+  - sudo echo -e '{\n "log-driver"': '"local",\n "registry-mirrors" : [ "https://dockerhub.timeweb.cloud" ]\n}' > /etc/docker/daemon.json
   - sudo systemctl daemon-reload
   - sudo systemctl restart docker.service
 `)
